846c1e45fa59d7b1c4516790014f9c16bd3a77f7
Dan Spencer <dspence180@gmail.com>
2022-07-30 09:45:03 -0500

    finished implementing controller for workouts routes

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -13,7 +13,7 @@ const getWorkout = async (req, res) => {
   const { id } = req.params;
 
   if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(404).json({ error: 'No such workout' });
+    return res.status(400).json({ error: 'No such workout' });
   }
 
   const workout = await Workout.findById(id);
@@ -38,13 +38,43 @@ const createWorkout = async (req, res) => {
 }
 
 // delete a workout
+const deleteWorkout = async (req, res) => {
+  const { id } = req.params;
 
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ error: 'No such workout' });
+  }
+
+  const workout = await Workout.findOneAndDelete({ _id: id });
+
+  if (!workout) {
+    return res.status(404).json({ error: 'No such workout' });
+  }
+
+  res.status(200).json(workout);
+}
 
 // update a workout
+const updateWorkout = async (req, res) => {
+  const { id } = req.params;
 
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ error: 'No such workout' });
+  }
+
+  const workout = await Workout.findOneAndUpdate({ _id: id }, { ...req.body });
+
+  if (!workout) {
+    return res.status(404).json({ error: 'No such workout' });
+  }
+
+  res.status(200).json(workout);
+}
 
 module.exports = {
   getAllWorkouts,
   getWorkout,
   createWorkout,
+  deleteWorkout,
+  updateWorkout,
 }
