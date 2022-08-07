49fdf622442e70c0b987ee1c4511849ba0414cb3
Dan Spencer <dspence180@gmail.com>
2022-08-07 13:59:04 -0500

    added handling for deleting workouts

--- frontend/src/components/WorkoutDetails.jsx
+++ frontend/src/components/WorkoutDetails.jsx
@@ -1,10 +1,27 @@
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext';
+
 const WorkoutDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutsContext();
+
+  const handleClick = async () => {
+    const response = await fetch('/api/workouts/' + workout._id, {
+      method: 'DELETE',
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      dispatch({ type: 'DELETE_WORKOUT', payload: data });
+    }
+  }
+
   return (
     <div className="workout-details">
       <h4>{workout.title}</h4>
       <p><strong>Load (lbs): </strong>{workout.load}</p>
       <p><strong>Reps: </strong>{workout.reps}</p>
       <p><small>{workout.createdAt}</small></p>
+      <span onClick={handleClick} title="delete workout">x</span>
     </div>
   );
 }
