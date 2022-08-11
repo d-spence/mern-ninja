a189a6d6ea342a0a741f63c551155b3f580a1ca9
Dan Spencer <dspence180@gmail.com>
2022-08-11 16:02:23 -0500

    basic setup for express user routes

---
+++ backend/controllers/userController.js
@@ -0,0 +1,14 @@
+// login user
+const loginUser = async (req, res) => {
+  res.status(501).json({ msg: 'NOT IMPLEMENTED: login user' });
+}
+
+// register user
+const registerUser = async (req, res) => {
+  res.status(501).json({ msg: 'NOT IMPLEMENTED: register user' });
+}
+
+module.exports = {
+  loginUser,
+  registerUser,
+}
