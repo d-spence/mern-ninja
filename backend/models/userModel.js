5825fff32e202eff06e4260526c62bca6ce1d801
Dan Spencer <dspence180@gmail.com>
2022-08-11 18:05:54 -0500

    email and password validation

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const validator = require('validator');
 
 const Schema = mongoose.Schema;
 
@@ -17,6 +18,30 @@ const userSchema = new Schema({
 
 // static register method
 userSchema.statics.register = async function(email, password) {
+  // validation
+  if (!email || !password) {
+    throw Error('All fields must be filled');
+  }
+
+  if (!validator.isEmail(email)) {
+    throw Error ('Email is not valid');
+  }
+
+  const validatorPasswordOptions = {
+    minLength: 6,
+    minLowercase: 1,
+    minUppercase: 0,
+    minNumbers: 1,
+    minSymbols: 0,
+  };
+
+  if (!validator.isStrongPassword(password, validatorPasswordOptions)) {
+    const { minLength } = validatorPasswordOptions;
+
+    throw Error (`Password must be valid and contain at least ${minLength} characters`);
+  }
+
+  // check if the user email already exists
   const userExists = await this.findOne({ email });
 
   if (userExists) {
